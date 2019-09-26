784178c6440c13644faa72f9c92d4f7868bbbbe1
Yevgeny Pats <yp@fuzzit.dev>
2019-09-26 16:48:34 +0300

    bump to v2.4.58

--- client/client.go
+++ client/client.go
@@ -11,7 +11,7 @@ import (
 )
 
 const FuzzitEndpoint = "https://app.fuzzit.dev"
-const Version = "v2.4.57"
+const Version = "v2.4.58"
 
 type Target struct {
 	Name         string `firestore:"target_name"`
